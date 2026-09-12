8ea8ab07198f5fe9289e8a4267d2ce4fdf6a3d4f
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-12 22:11:21 +0300

    Improve Vihry 3000 vacuum model and material reflections

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = '122b6c26c6';
+const VERSION = '95fca84f23';
 const PREFIX = "phantom-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
